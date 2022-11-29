f7adc80e8c4fa4a83bca047235b0d30fc7755d2e
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2022-11-29 23:03:04 +0900

    3.5.1 useState

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import ContainerSample from './components/ContainerSample'
 import Page from './components/ContextSample'
+import Counter from './components/StateSample'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <ContainerSample></ContainerSample>
       <Page></Page>
+      <Counter initialValue={0}></Counter>
     </div>
   )
 }
